e6e04ae4832306b81b2a04d37f2deb897bdb9666
Ved <65692297+Ved-programmer@users.noreply.github.com>
2020-10-09 09:54:54 +0530

    Delete signup.py

--- Login system/signup.py
+++
@@ -1,9 +0,0 @@
-from hashing import *
-import checks
-def addToIndex(username, password):
-    result = checks.check(username, password, False)
-    if result.accepted:
-        password = revert_back(increaseStringSize(password),logic(password))
-        with open("data.txt", "a") as f:
-            f.write(f"{username}-{password}\n")
-    return result
