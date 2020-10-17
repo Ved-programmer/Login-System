3ffa34b4a9aabc93c54f0a8fbe7f09714f14ace6
Ved <65692297+Ved-programmer@users.noreply.github.com>
2020-10-17 13:53:46 +0530

    Add files via upload

--- Login system/checks.py
+++ Login system/checks.py
@@ -1,15 +1,12 @@
 from backEndUtilityFuncs import usernameExists
 def check(username, password, passChange):
-    if not passChange:
-        try:assert not(usernameExists(username))
-        except Exception:return message(False, f"The username {username} is unavailable, please try a different username")
-    else:
-        try:assert usernameExists(username)
-        except Exception:return message(False, f"The username {username} does not exist please re-check this username")
-    
     securePasswordChecks = [passLenCheck, containsProperChars]
+    usernameChecks = [usernameCharCheck, usernameExistsCheck]
     for check in securePasswordChecks:
         result = check(password)
+        if not result.accepted:return result
+    for check in usernameChecks:
+        result = check(username, passChange)
         if not result.accepted:return result
     return message(True, "Password accepted")
 
@@ -32,6 +29,18 @@ def containsProperChars(password):
     if number and symbol and upperCase and lowerCase:return message(True, "This password is accepted")
     return message(False, "The message must contain a number, a symbol, an uppercase and a lowercase letter.")
 
+def usernameCharCheck(username, passChange = False):
+    if "-" in username: return message(False, "Username can't contain '-' character")
+    return message(True, "Username accepted")
+
+def usernameExistsCheck(username, passChange):
+    if not passChange:
+        try:assert not(usernameExists(username))
+        except Exception:return message(False, f"The username {username} is unavailable, please try a different username")
+    else:
+        try:assert usernameExists(username)
+        except Exception:return message(False, f"The username {username} does not exist please re-check this username")
+    return message(True, "Username Available")
 
 
 class message:
