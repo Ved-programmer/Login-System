2e101353539156637b6458814f83d9e90e85cfca
Ved Rathi <65692297+Ved-programmer@users.noreply.github.com>
2021-10-03 08:53:57 +0530

    Update showAbout.py

--- Login system/showAbout.py
+++ Login system/showAbout.py
@@ -1,13 +1,14 @@
 from tkinter import *
 import webbrowser
-def main(root):
-    HEIGHT = 502
-    WIDTH = 612
+import tkinter.font as tkFont
 
+def main(root, WIDTH, HEIGHT, wu, hu):
     win = Frame(root, bg = "black")
     win.place(x = 0, y = 0, width = WIDTH, height = HEIGHT)
 
-    Label(win, text = "About The creators", font = (" ", int(30)), fg = "white", bg = "black").place(x = WIDTH//2, y = 100, anchor = "center")
+    
+    
+    Label(win, text = "About The creators", font = tkFont.Font(size = -int(HEIGHT/8)), fg = "white", bg = "black").place(x = WIDTH//2, y = 100*hu, anchor = "center")
 
     string = """
     The first version of the project was created by 
@@ -18,11 +19,16 @@ def main(root):
         win.place_forget()
         root.quit()
 
-    Label(win, text = string, font = (" ", int(15)), justify = "left").place(x = WIDTH//2, y = HEIGHT//2, anchor = "center")
+    Label(win, text = string, font = tkFont.Font(size = -int(HEIGHT/13)), justify = "left").place(x = WIDTH//2, y = HEIGHT//2, anchor = "center")
 
-    Button(win, text = "Go Back", font = (" ", int(15)), justify = "center", bg = "red", command = back).place(x = 0, y = 0)
-    Button(win, text = "Ved's Github", font = (" ", int(15)), justify = "center", bg = "red", command = lambda : webbrowser.open("https://github.com/Ved-programmer")).place(x = 50, y = 400, width = 250)
-    Button(win, text = "Navdeep's Github", font = (" ", int(15)), justify = "center", bg = "red", command = lambda : webbrowser.open("https://github.com/smartnavdeep")).place(x = 312, y = 400, width = 250)
+    Button(win, text = "Go Back", font = tkFont.Font(size = -int(HEIGHT/15)), justify = "center", bg = "red", command = back).place(x = 0, y = 0)
+
+    yPos = 700*hu
+    width = 400*wu
+    margin = 75*wu
+
+    Button(win, text = "Ved's Github", font = tkFont.Font(size = -int(HEIGHT/13)), justify = "center", bg = "red", command = lambda : webbrowser.open("https://github.com/Ved-programmer")).place(x = margin, y = yPos, width = width)
+    Button(win, text = "Navdeep's Github", font = tkFont.Font(size = -int(HEIGHT/13)), justify = "center", bg = "red", command = lambda : webbrowser.open("https://github.com/crypto-navdeep")).place(x = WIDTH - margin - width, y = yPos, width = width)
 
     root.mainloop()
 
