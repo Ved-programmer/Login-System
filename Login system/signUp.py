747e31be910a0fff2d82ea8f9a3bf5679214a31b
Ved Rathi <65692297+Ved-programmer@users.noreply.github.com>
2021-10-04 14:58:04 +0530

    Update signUp.py

--- Login system/signUp.py
+++ Login system/signUp.py
@@ -2,8 +2,10 @@ from hashing import encrypt
 import checks
 def addToIndex(username, password):
     result = checks.check(username, password, False)
+    # print(result)
     if result.accepted:
         password = encrypt(password)
         with open("data.txt", "a") as f:
             f.write(f"{username}-{password}\n")
     return result
+    
